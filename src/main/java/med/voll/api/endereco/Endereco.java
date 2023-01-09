5a8860f08c90688e4fa0c5a6d3b60c3a0fcff99b
valeriajanzen <janzen_valeria@outlook.com>
2023-01-09 17:46:53 -0300

    adc telefone

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,8 +1,14 @@
 package med.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
     private String logradouro;
     private String bairro;
@@ -12,8 +18,9 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+
     public Endereco(DadosEndereco dados) {
-        this.logradouro = dados.Logradouro();
+        this.logradouro = dados.logradouro();
         this.bairro = dados.bairro();
         this.cep = dados.cep();
         this.uf = dados.uf();
@@ -21,4 +28,5 @@ public class Endereco {
         this.numero = dados.numero();
         this.complemento = dados.complemento();
     }
+
 }
