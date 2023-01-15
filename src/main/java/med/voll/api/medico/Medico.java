9a4630007dc23e29b9f0c3874caf4fbb0fedf665
valeriajanzen <janzen_valeria@outlook.com>
2023-01-15 15:47:59 -0300

    Adicionando metodos PUT e criando novos medotos DTO para atualização de cadastro

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -40,4 +40,16 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
 
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
